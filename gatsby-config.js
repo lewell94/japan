06957f7e3c39b15f87acdca38852e5ef0042cc11
Elliot Lewis <elliotlewis@TENPDLAPTOP059.local>
2019-02-26 17:04:33 +0000

    get contentful tokens from env vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `xxv139uj2trn`,
-        accessToken: `2e82e6c1af372169b71bb51f92f08a5e22ae2aad0da862f090a53211aeedf2ed`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
